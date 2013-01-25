27afd7d06b201e0220bd39b29aa2662d8b014a7b
Chad Estioco <chad@galadriel.(none)>
2013-01-26 03:07:14 +0800

    Fixed OO syntax in Graphs superclass.

--- data_structures/graphs.py
+++ data_structures/graphs.py
@@ -19,7 +19,7 @@ class Graph(object):
     be defined for the nodes.
     """
 
-    def is_reachable(n1, n2):
+    def is_reachable(self, n1, n2):
         """
         Returns true if we can go to n2 via n1. Note
         that if the graph is directed, is_reachable(n1, n2)
@@ -30,26 +30,26 @@ class Graph(object):
         """
         pass
 
-    def get_neighbors(n1):
+    def get_neighbors(self, n1):
         """
         Returns all nodes reachable via n1.
         """
         pass
     
-    def add_node(node):
+    def add_node(self, node):
         """
         Adds this node to the graph.
         """
         pass
     
-    def remove_node(node):
+    def remove_node(self, node):
         """
         Removes the given node from the graph. All incoming
         connections to this node are removed as well.
         """
         pass
 
-    def make_neighbor(n1, n2):
+    def make_neighbor(self, n1, n2):
         """
         Makes n2 reachable from n1 (but not necessarily n1
         from n2).
