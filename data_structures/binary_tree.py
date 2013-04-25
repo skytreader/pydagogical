7d86091f4a78c6124f114fa17eda3e6e78116f69
skytreader <chadestioco@gmail.com>
2013-04-25 15:37:16 +0800

    Preliminary binary tree source code

---
+++ data_structures/binary_tree.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python3
+
+import unittest
+
+"""
+Naive binary tree implementation.
+"""
+
+class BinaryTree(object):
+    """
+    Interface for binary tree objects.
+    """
+    
+    """
+    Flag for depth-first search.
+    """
+    DFS = "dfs"
+    """
+    Flag for breadth-first search.
+    """
+    BFS = "bfs"
+    """
+    Flag for in-order search
+    """
+    INP = "inp"
+
+    def __init__(self, node_data):
+        self.node_data = node_data
+        self.left_son = None
+        self.right_son = None
+
+    def search(self, query, search_type = ""):
+        """
+        Searches the binary tree for the given query data.
+        Returns either true or false. The optional search_type
+        parameter can be used to specify the type of search
+        to be done. Use the DFS, BDS, and INP fields of this
+        class.
+        """
+        pass
+
+class DFSIterator(object):
+    
+    def __init__(self, bintree):
+        self.bintree = bintree
+        # Always point to the current node
+        self.roving_pointer = self.bintree
+        # the traversal stack
+        self.traversal_stack = []
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        """
+        Return what comes next and update iterator's internal
+        state to point to the DFS successor of the current node.
+        """
+        if self.bintree.left_node is None:
+            self.roving_pointer = self.traversal_stack.pop()
+            return self.roving_pointer
+
+class NaiveBinaryTree(BinaryTree):
+    """
+    Naive implementation of a binary tree.
+    """
+
+    def __init__(self, node_data):
+        super(NaiveBinaryTree, self).__init__(node_data)
+
+    def search(self, query, search_type = BinaryTree.DFS):
+        """
+        Searches using depth-first search by default.
+        """
+
+class NaiveBinaryTreeTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.bt1 = NaiveBinaryTree("root")
+    
+    def test_init(self):
+        self.assertEqual(self.bt1.left_son, None)
+        self.assertEqual(self.bt1.right_son, None)
+
+if __name__ == "__main__":
+    unittest.main()
