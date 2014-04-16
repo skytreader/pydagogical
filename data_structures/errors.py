136bf3ab59b6a33b98522d2e42ccb233888f549a
Chad Estioco <chadestioco@gmail.com>
2014-04-16 10:59:11 +0800

    Added new exception CorruptedStructureException.
    Use it for when an invariant about a structure suddenly fails to hold.

--- data_structures/errors.py
+++ data_structures/errors.py
@@ -17,3 +17,21 @@ class DuplicateNodeException(Exception):
 
     def __str__(self):
         return "Already added to the structure: " + repr(self.value)
+
+class CorruptedStructureException(Exception):
+    """
+    Thrown when suddenly a given invariant for a structure fails to hold.
+
+    This is almost always programmer error. Concurrency bug? Sloppy programming?
+    I'd check those.
+    """
+    
+    def __init__(self, value):
+        """
+        The value is expected to be the class of the structure that was
+        corrupted. Otherwise, you may not get meaningful exception messages.
+        """
+        self.value = value
+
+    def __str__(self):
+        return "Corrupted structure: " + str(self.value)
