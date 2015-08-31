985bf3e03bbddf6617c63e267a8dc56dd4010875
Chad Estioco <chadestioco@gmail.com>
2015-08-31 21:06:30 +0800

    Procedure terminates but answer is incorrect.

--- algorithms/number_theory.py
+++ algorithms/number_theory.py
@@ -4,7 +4,6 @@ def pulverize(a, b):
     as + tb = gcd(a, b). The sequence of the return value corresponds to their
     multipliers in the expression.
     """
-    print("here")
     s = 0
     _s = 1
 
@@ -15,9 +14,8 @@ def pulverize(a, b):
     _r = a
 
     while r:
-        print(r)
         q = int(_r / r)
-        _r, r = r, _r - q * s
+        _r, r = r, _r - q * r
         _s, s = s, _s - q * s
         _t, t = t, _t - q * t
 
