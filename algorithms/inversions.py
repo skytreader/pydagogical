f5579bceef77141069ca3cf52a20db581e3b5bbd
Chad Estioco <chadestioco@gmail.com>
2016-02-07 23:50:13 +0800

    Refactor tests for inversions.

--- algorithms/inversions.py
+++ algorithms/inversions.py
@@ -138,29 +138,3 @@ def merge_inversion_count(numlist):
         i *= 2
 
     return inversion_count
-
-
-class FunctionsTest(unittest.TestCase):
-    
-    def test_naive(self):
-        self.assertEqual(5, naive_inversion_count([2, 3, 8, 6, 1]))
-        self.assertEqual(5, naive_inversion_count([1, 4, 1, 5, 9, 2, 6]))
-        self.assertEqual(6, naive_inversion_count([4, 1, 5, 2, 6, 3]))
-        self.assertEqual(60, naive_inversion_count((0,15,14,1,13,2,3,12,11,4,5,10,6,9,8,7)))
-
-    def test_merge_inversion_count(self):
-        self.assertEqual(5, merge_inversion_count([2, 3, 8, 6, 1]))
-        self.assertEqual(5, merge_inversion_count([1, 4, 1, 5, 9, 2, 6]))
-        self.assertEqual(6, merge_inversion_count([4, 1, 5, 2, 6, 3]))
-        self.assertEqual(60, merge_inversion_count((0,15,14,1,13,2,3,12,11,4,5,10,6,9,8,7)))
-        self.assertEqual(0, merge_inversion_count([]))
-        self.assertEqual(0, merge_inversion_count([1]))
-        self.assertEqual(0, merge_inversion_count([1, 2, 3]))
-
-        spam = [2, 3, 8, 6, 1]
-        self.assertEqual(5, merge_inversion_count(spam))
-        self.assertEqual([2, 3, 8, 6, 1], spam)
-
-
-if __name__ == "__main__":
-    unittest.main()
