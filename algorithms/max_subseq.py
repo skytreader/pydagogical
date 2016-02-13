cbf3b05653a902f8f31e534f8674d344578b0759
Chad Estioco <chadestioco@gmail.com>
2016-02-13 23:43:35 +0800

    One of my favorite problems refactored.
    Must add my notes here too.

--- algorithms/max_subseq.py
+++ algorithms/max_subseq.py
@@ -114,24 +114,3 @@ def find_max_subarray(numseq, low, high):
 def max_subarray(numseq):
     """Driver method. Call this not find_max_subarray"""
     return find_max_subarray(numseq, 0, len(numseq) - 1)
-
-class FunctionsTest(unittest.TestCase):
-    
-    def test_find_biased_max(self):
-        self.assertEqual((6, 45), find_biased_max((5, 15, -30, 10, -5, 40, 10), 4, 6))
-
-    def test_max_subarray(self):
-        self.assertEqual((0, 1, 20), max_subarray((5, 15, -30, 10)))
-        # The example from the book
-        stocks = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
-        self.assertEqual((7, 10, 43), max_subarray(stocks))
-        self.assertEqual((0, 1, 10), max_subarray((7, 3)))
-        self.assertEqual((1, 1, 10), max_subarray((-10, 10)))
-        self.assertEqual((0, 0, 10), max_subarray((10, -10)))
-        self.assertEqual((0, 0, 10), max_subarray([10]))
-        self.assertEqual((1, 2, 50), max_subarray((-5, 40, 10)))
-        self.assertEqual((0, 0, -10), max_subarray([-10]))
-        self.assertEqual((3, 6, 55), max_subarray((5, 15, -30, 10, -5, 40, 10)))
-
-if __name__ == "__main__":
-    unittest.main()
