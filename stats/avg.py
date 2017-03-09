53ddd8d2b15a1378a6b256c3079887b8dc247234
Chad Estioco <chadestioco@gmail.com>
2017-03-10 05:41:09 +0800

    Add stats package.

---
+++ stats/avg.py
@@ -0,0 +1,13 @@
+import math
+
+def mean(numset):
+    return sum(numset) / len(numset)
+
+def variance(numset):
+    m = mean(numset)
+    setsize = len(numset)
+    varterms = (( ((x - m) ** 2) / setsize) for x in numset)
+    return sum(varterms)
+
+def standard_deviation(numset):
+    return math.sqrt(variance(numset))
