a2e4cdcdf47ab56a2744d75e56d20b1c8035b3e5
Chad Estioco <chadestioco@gmail.com>
2017-05-01 18:43:01 +0800

    savepoint

---
+++ stats/tests/expectation_tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+from stats import expectation as exp
+
+class FunctionsTests(unittest.TestCase):
+
+    def test_cardinality_check(self):
+        val_table = (
+            (-2, 23, 48, 73),
+            (0.977, 0.008, 0.008, 0.006, 0.001)
+        )
+        self.assertRaises(
+            ValueError,
+            self.__cardinality_check,
+            val_table[0],
+            val_table[1]
+        )
+
+    def test_expected_value(self):
+        val_table = (
+            (-2, 23, 48, 73, 98),
+            (0.977, 0.008, 0.008, 0.006, 0.001)
+        )
+        self.assertAlmostEqual(-0.85, exp.expected_value(**val_table))
