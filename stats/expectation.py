5a5986b794d4cc010489a7efa6ddc09103fe52ee
Chad Estioco <chadestioco@gmail.com>
2017-05-01 18:59:31 +0800

    Add variance tests to expectations.

--- stats/expectation.py
+++ stats/expectation.py
@@ -8,3 +8,8 @@ def _cardinality_check(events, p_ev):
 def expected_value(events, p_ev):
     _cardinality_check(events, p_ev)
     return sum((x[0] * x[1] for x in zip(events, p_ev)))
+
+def ev_variance(events, p_ev):
+    exp_val = expected_value(events, p_ev)
+    new_events = tuple(((x - exp_val) ** 2 for x in events))
+    return expected_value(new_events, p_ev)
--- stats/tests/expectation_tests.py
+++ stats/tests/expectation_tests.py
@@ -22,3 +22,10 @@ class FunctionsTests(unittest.TestCase):
             (0.977, 0.008, 0.008, 0.006, 0.001)
         )
         self.assertAlmostEqual(-0.85, exp.expected_value(val_table[0], val_table[1]))
+
+    def test_ev_variance(self):
+        val_table = (
+            (-2, 23, 48, 73, 98),
+            (0.977, 0.008, 0.008, 0.006, 0.001)
+        )
+        self.assertAlmostEqual(67.4275, exp.ev_variance(val_table[0], val_table[1]))
