2fcefb4f89f1e837b54512f8957de9bc33147ed9
Chad Estioco <chadestioco@gmail.com>
2017-05-04 05:04:21 +0800

    More experiments. Bye coverage.

--- algorithms/misc.py
+++ algorithms/misc.py
@@ -2,8 +2,9 @@
 
 def get_non_decreasing_subranges(lnum):
     """
-    Return inclusive subranges that are non-decreasing within lnum. Thecontinuity
-    of subranges is based on the longest possible chains of non-decreasing subranges.
+    Return inclusive subranges that are non-decreasing within lnum. The
+    continuity of subranges is based on the longest possible chains of
+    non-decreasing subranges.
     """
     i = 1
     subrange_start = 0
@@ -12,6 +13,27 @@ def get_non_decreasing_subranges(lnum):
 
     while i < limit:
         if lnum[i] < lnum[i - 1]:
+            subranges.add((subrange_start, i - 1))
+            subrange_start = i
+        i += 1
+
+    if lnum and lnum[subrange_start] <= lnum[i - 1]:
+        subranges.add((subrange_start, i - 1))
+    
+    return subranges
+
+def get_proper_non_decreasing_subranges(lnum):
+    """
+    Same as get_non_decreasing_subranges above but will return non-trivial
+    subranges: i.e., subranges that are not singletons.
+    """
+    i = 1
+    subrange_start = 0
+    limit = len(lnum)
+    subranges = set()
+
+    while i < limit:
+        if lnum[i] < lnum[i - 1] and subranges != (i - 1):
             subranges.add((subrange_start, i - 1))
             subrange_start = i
         i += 1
