29ce9231c26888928a5fd834da42472178fc35dd
Chad Estioco <chadestioco@gmail.com>
2017-05-27 18:43:05 +0800

    Find liars through unit tests.

--- algorithms/tests/misc_tests.py
+++ algorithms/tests/misc_tests.py
@@ -15,3 +15,12 @@ class MiscTests(unittest.TestCase):
             set([(0, 0), (1, 1), (2, 2)]),
             get_non_decreasing_subranges([3, 2, 1])
         )
+
+    def test_proper_non_decreasing_subranges(self):
+        self.assertEqual(set(), get_proper_non_decreasing_subranges([]))
+        self.assertEqual(set([(0, 2)]), get_proper_non_decreasing_subranges([1, 2, 3]))
+        self.assertEqual(
+            set([(0, 2)]),
+            get_proper_non_decreasing_subranges([1, 2, 3, 2, 1, 0])
+        )
+        self.assertEqual(set(), get_proper_non_decreasing_subranges([3, 2, 1]))
