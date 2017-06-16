3df32bbabe785ce1ce3654f6e75259db7c469a0e
Chad Estioco <chadestioco@gmail.com>
2017-06-17 06:17:51 +0800

    Make tests pass.

--- music_theory/scales.py
+++ music_theory/scales.py
@@ -4,10 +4,10 @@ INTERVAL_PATTERN = [2, 2, 1, 2, 2, 2, 1]
 def construct_major_scale(unison_note):
     norm_note = unison_note.upper()
     note_index = CHROMATIC_SCALE.index(norm_note)
-    major_scale = [unison_note]
+    major_scale = [norm_note]
 
     for step in INTERVAL_PATTERN:
-        note_index = (note_index + step) % len(INTERVAL_PATTERN)
+        note_index = (note_index + step) % len(CHROMATIC_SCALE)
         major_scale.append(CHROMATIC_SCALE[note_index])
 
     return major_scale
