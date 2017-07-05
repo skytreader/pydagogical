8cbc5028dd48e4f1f206080115ad1a5ac3d5c62d
Chad Estioco <chadestioco@gmail.com>
2017-07-05 23:37:13 +0800

    Works but lacking.

--- algorithms/permutations.py
+++ algorithms/permutations.py
@@ -3,10 +3,18 @@ All methods take in an iterable and returns a set of tuples. The sequences are
 not expected to be unique.
 """
 
-def __reconstruct_original_chars(index_perms, seq):
-    return [[seq[index] for index in permutation] for permutation in index_perms]
-    for permutation in indices:
-        perm = 
+def __reconstruct_original_chars(index_perm, seq):
+    return [seq[index] for index in index_perm]
+
+def generate_next_state(curstate, limit):
+    """
+    Assumes that len(curstate) != limit.
+    """
+    next_state = curstate
+    for i in range(limit):
+        if i not in curstate:
+            next_state.append(i)
+            return next_state
 
 def permute_dfs(seq):
     if not seq:
@@ -15,11 +23,20 @@ def permute_dfs(seq):
     permutations = set()
     seqlimit = len(seq)
     cur_index = 0
-    genstack = [0]
+    genstack = [[cur_index]]
 
-    while cur_index < seqlimit and genstack:
+    while cur_index < seqlimit:
         cur_prefix = genstack.pop()
+        print(cur_prefix)
+        next_state = generate_next_state(cur_prefix, seqlimit)
 
-        
+        if len(next_state) == seqlimit:
+            permutations.add(tuple(__reconstruct_original_chars(next_state, seq)))
+        else:
+            genstack.append(next_state)
+
+        if not genstack:
+            cur_index += 1
+            genstack = [[cur_index]]
 
     return permutations
