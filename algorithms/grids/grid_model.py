6d21ee780612bd4307fe2782cabeb55991ce1ff6
Chad Estioco <chadestioco@gmail.com>
2018-03-09 16:43:50 +0800

    The window shows but does not (yet) display as expected.

--- algorithms/grids/grid_model.py
+++ algorithms/grids/grid_model.py
@@ -1,5 +1,6 @@
 from components.core import GameModel
 from components.helpers.grid import BorderProperties, QuadraticGrid
+from screen import GridScreen
 
 class Grid(GameModel):
 
@@ -7,13 +8,23 @@ class Grid(GameModel):
     BLOCKED = False
 
     def __init__(self, grid_size):
+        super(Grid, self).__init__()
         self.qg = QuadraticGrid(
             grid_size[0], grid_size[1], diag_neighbors=False,
             border_properties=BorderProperties()
         )
-        for i in range(width):
-            for j in range(height):
-                self.qg.grid[i][j] = FREE
+        for i in range(grid_size[0]):
+            for j in range(grid_size[1]):
+                self.qg.grid[i][j] = GridScreen.UNTAKEN
 
     def toggle(self, row, col):
         self.qg.grid[row][col] = not self.qg.grid[row][col]
+    
+    def render(self, **kwargs):
+        row = kwargs["row"]
+        col = kwargs["col"]
+
+        if self.qg.grid[row][col]:
+            return GridScreen.UNTAKEN
+        else:
+            return GridScreen.BLOCKED
--- algorithms/grids/screen.py
+++ algorithms/grids/screen.py
@@ -1,6 +1,13 @@
 from components.core import GameScreen
+from components.core import Colors
 
 class GridScreen(GameScreen):
 
+    UNTAKEN = Colors.MAX_WHITE
+    BLOCKED = Colors.ADVENTURE_DARK
+
     def __init__(self, config, model):
         super(GridScreen, self).__init__(config, model)
+
+    def draw_screen(self, window):
+        self.model.qg.draw(window, self)
