d18d74fe27ce3d88fb39f630f02ac04a9a088805
Chad Estioco <chadestioco@gmail.com>
2018-03-11 05:57:19 +0800

    Get psychedelic.

--- algorithms/grids/main.py
+++ algorithms/grids/main.py
@@ -3,7 +3,7 @@ from grid_model import Grid
 from screen import GridScreen
 
 if __name__ == "__main__":
-    config = GameConfig()
+    config = GameConfig(window_size=(600, 600))
     model = Grid((10, 10))
     screen = GridScreen(config, model)
     loop_events = GameLoopEvents(config, screen)
