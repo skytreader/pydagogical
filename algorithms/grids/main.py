730a9f9b9c310bdcce797bf50a09a6ded6262409
Chad Estioco <chadestioco@gmail.com>
2018-03-16 05:30:47 +0800

    Debugging no longer needed (for now).

--- algorithms/grids/main.py
+++ algorithms/grids/main.py
@@ -3,7 +3,7 @@ from grid_model import Grid
 from screen import GridLoopEvents, GridScreen
 
 if __name__ == "__main__":
-    config = GameConfig(window_size=(600, 600), debug_mode=True, log_to_terminal=True)
+    config = GameConfig(window_size=(600, 600))
     model = Grid((10, 10))
     screen = GridScreen(config, model)
     loop_events = GridLoopEvents(screen)
