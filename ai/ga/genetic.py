ef78354d871e2327667dcfbf2b384e19bbc95689
Chad Estioco <chadestioco@gmail.com>
2018-05-10 05:43:11 +0800

    Implement SGA according to [PREBYS].

--- ai/ga/genetic.py
+++ ai/ga/genetic.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 class SolutionStat(object):
 
@@ -43,6 +44,9 @@ class GASolver(object):
     def create_offspring(self):
         """
         Adds mutations to current_pool.
+
+        This _desperately_ tries to create an offspring with a better fitness
+        than its parents.
         """
         cur_max_fitness = max(self.__compute_generation_fitness())
 
@@ -83,6 +87,104 @@ class GASolver(object):
             if solution is None:
                 self.create_offspring()
                 self.__conditional_print("Fitness of current_pool: %s" % self.__compute_generation_fitness())
+
+            itercount += 1
+
+        if solution is None:
+            max_variation = self.current_pool[0]
+            max_var_fitness = self.compute_fitness(max_variation)
+
+            for variation in self.current_pool[1:]:
+                cand_var_fitness = self.compute_fitness(max_variation)
+                if self.compute_fitness(variation) > cand_var_fitness:
+                    max_variation = variation
+                    max_var_fitness = cand_var_fitness
+
+            print("Exhausted possibilities. Best answer has score: %s" % self.compute_fitness(max_variation))
+            return SolutionStat(
+                answer=max_variation, ans_score=max_var_fitness,
+                iters=itercount, max_iters=self.max_iterations
+            )
+        else:
+            return SolutionStat(
+                answer=solution, ans_score=self.compute_fitness(solution),
+                iters=itercount, max_iters=self.max_iterations
+            )
+
+class StandardGASolver(GASolver):
+    """
+    Implementation of an SGA based on [PREBYS].
+
+    Differences:
+    - [PREBYS] suggests to drop chromosomes in the event that population size is
+      odd. We take a more merciful approach and just make room for _another_
+      chromosome.
+    - Most decisions here still work on the assumption that this will be used to
+      solve instances of Mastermind.
+    """
+
+    def __init__(self, initial_pool, max_iterations=float("inf")):
+        max_pool_size = len(initial_pool)
+        if max_pool_size == 1:
+            raise ValueError("We need an initial pool of at least 2 to make children.")
+
+        if max_pool_size % 2:
+            max_pool_size += 1
+
+        super().__init__(initial_pool, max_iterations=max_iterations, max_pool_size=max_pool_size)
+
+    def __crossover(self, parents):
+        division_limit = random.randint(1, len(parents[0] - 1))
+
+        p0a = parents[0][:division_limit]
+        p0b = parents[0][division_limit:]
+
+        p1a = parents[1][:division_limit]
+        p1b = parents[1][division_limit:]
+
+        p0a.extend(p1b)
+        p1a.extend(p0b)
+
+        return (p0a, p1a)
+
+    def solve(self):
+        # This iteration limiting is not in [PREBYS] and yet we do them because
+        # they make sense from an engineering perspective.
+        solution = None
+        itercount = 0
+        
+        new_generation = initial_pool
+
+        while solution is None and itercount < self.max_iterations:
+            self.current_pool = new_generation
+            new_generation = []
+
+            while len(new_generation) < self.max_pool_size:
+                generation_fitness = self.__compute_generation_fitness()
+                individual_fitness_map = zip(self.current_pool, generation_fitness)
+                individual_fitness_map.sort(key=lambda x: x[1], reverse=True)
+                # Update current_pool for culling
+                self.current_pool = [x[0] for x in individual_fitness_map]
+
+                # This is a very austere way of choosing the new generation...we should
+                # look into using a probabilistic selection function.
+                chosen_parents = [individual_fitness_map[0][0], individual_fitness_map[1][0]]
+                # Cull the pool
+                self.current_pool = self.current_pool[2:]
+
+                # Randomly decide if we should crossover
+                children = None
+                if random.choice((True, False)):
+                    children = self.__crossover(chosen_parents)
+                else:
+                    children = chosen_parents
+
+                new_generation.extend(children)
+
+            for child in new_generation:
+                if self.compute_fitness(child) == 1:
+                    solution = child
+                    break
 
             itercount += 1
 
