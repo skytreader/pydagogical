91ff8786a4052dcb8d1df461478bae84990b8a94
Chad Estioco <chadestioco@gmail.com>
2018-12-24 03:11:18 +0800

    Rapid prototyping the experiment design of exchange gift.

--- experimental-statistics/exchange-gift/main.py
+++ experimental-statistics/exchange-gift/main.py
@@ -5,19 +5,19 @@ Problem statement: It's Christmas and Alice and Bob's office is having an
 exchange gift activity. To facilitate this, every person in the office will draw
 a name from the pool; this will be the person to whom they will give their gift.
 
-However, for some reason or another, Alice does not want to draw Bob, Alice
+However, for some reason or another, Alice does not want to draw Bob. Alice
 accepts that it can't be avoided completely but she wonders _when_ should she
 draw a name to minimize her chances of drawing Bob. There are two factors to her
 decision:
 
-1. Is she is the nth person to draw a name, there is $1 / N - (n-1)$ chance of
+1. If she is the nth person to draw a name, there is $1 / N - (n-1)$ chance of
 drawing Bob, where N is the number of people in their office. Obviously, she
 minimizes her chance of drawing Bob by being the first person to draw. At that
 point there is $1 / N$ chance of drawing Bob and will only increase after.
 
 2. However, somewhere in the $n < N$ draws that will take place will come a
 point where someone else draws Bob's name. After that, Alice is completely safe
-as there is 0 probability of drawing Bob. So she chould minimize her chances by
+as there is 0 probability of drawing Bob. So she could minimize her chances by
 determining the average number of draws it would take to draw Bob and just
 drawing after that.
 
@@ -30,11 +30,52 @@ What strategy should Alice choose?
 
 import random
 
-class ExchangeGiftSimulator(object):
+ALICE = "Alice"
+BOB = "Bob"
+
+class ExchangeGiftStrategy(object):
 
     def __init__(self, participant_count, locrand=random):
-        self.participants = ["Participant %s" % d for d in range(participant_count - 1)]
+        self.participants = ["Participant %s" % d for d in range(participant_count - 2)]
+        self.participants.append(ALICE)
+        self.participants.append(BOB)
         self.random = locrand
 
     def make_alice_choose(self):
-        raise NotImplementedError("Please implement Alice's strategy for drawing.")
+        """
+        Implement a strategy for avoiding Bob and see if it is successful. Note
+        that this is just one trial among many that must be performed for a
+        valid experiment.
+
+        If successful, return True. False otherwise.
+        """
+        raise NotImplementedError("Please implement Alice's strategy for avoiding Bob.")
+
+class DrawFirstStrategy(ExchangeGiftStrategy):
+
+    def make_alice_choose(self):
+        chosen = self.random.choice(self.participants)
+        while chosen == ALICE:
+            chosen = self.random.choice(self.participants)
+
+        return chosen != BOB
+
+class ExperimentSimulator(object):
+
+    def __init__(self, strategy, runs=1000):
+        self.runs = runs
+        self.strategy = strategy
+    
+    def run(self):
+        success = 0
+        for i in range(self.runs):
+            if self.strategy.make_alice_choose():
+                success += 1
+
+        return success
+
+if __name__ == "__main__":
+    experiment = ExperimentSimulator(DrawFirstStrategy(100))
+    success = experiment.run()
+
+    print("%s / %s = %s" % (success, experiment.runs, success / experiment.runs))
