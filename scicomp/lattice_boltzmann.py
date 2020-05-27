6941e80d9481ca2db7b0dc6fb37030fd1df4eeaa
Chad Estioco <chadestioco@gmail.com>
2020-05-28 02:12:41 +0800

    Add Lattice Boltzmann sample. Obviously not mine.

---
+++ scicomp/lattice_boltzmann.py
@@ -0,0 +1,11 @@
+--2020-05-28 01:49:39--  https://physics.weber.edu/schroeder/fluids/LatticeBoltzmannDemo.py.txt
+Resolving physics.weber.edu (physics.weber.edu)... 137.190.2.159
+Connecting to physics.weber.edu (physics.weber.edu)|137.190.2.159|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 8358 (8.2K) [text/plain]
+Saving to: ‘LatticeBoltzmannDemo.py.txt’
+
+     0K ........                                              100% 59.8M=0s
+
+2020-05-28 01:49:41 (59.8 MB/s) - ‘LatticeBoltzmannDemo.py.txt’ saved [8358/8358]
+
